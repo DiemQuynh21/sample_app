45a222f7377daf3bd11240f98f5cde1c1d551c4e
DiemQuynh21 <59770109+DiemQuynh21@users.noreply.github.com>
2020-11-11 09:40:22 +0700

    Chapter 3, 4, 5 Static pages

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  scope "(:locale)", locale: /en|vi/ do
+    root "static_pages#home"
+    get "/help", to: "static_pages#help"
+  end
+  # Ex:- scope :active, -> {where(:active => true)}
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
